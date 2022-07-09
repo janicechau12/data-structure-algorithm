fc0a74e523db067a769e0bb8d10eaa6e77c08492
Janice Chau <janice.chau@hilti.com>
2022-07-10 00:34:56 +0530

    Leetcode 199 - Binary Tree BFS

--- leetcode/Tree/Medium/199.py
+++ leetcode/Tree/Medium/199.py
@@ -8,6 +8,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+import collections
+
+
 class Solution(object):
     def rightSideView(self, root):
         """
@@ -17,18 +20,21 @@ class Solution(object):
         if root is None:
             return []
         
-        # traverse to right most
-        list = []
-        self.rightUtil(root, list)
-        return list
+        # BFS
+        queue = collections.deque([root])
+        results = []
         
-    def rightUtil(self, root, list):
-        if root is None:
-            return
-        else:
-            list.append(root.val)
-            if root.right:
-                self.rightUtil(root.right, list)
-            else:
-                self.rightUtil(root.left, list)
+        while queue:
+            level_length = len(queue)
+            
+            for i in range(level_length):
+                node = queue.popleft()
+                if i == level_length - 1:
+                    results.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                
+        return results
             
